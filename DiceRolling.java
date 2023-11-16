c615bb456d7a3e74a9680bf016d55ad1c9fafce3
Apex Incarna <106893568+Apex-Incarna@users.noreply.github.com>
2023-11-16 10:33:53 -0500

    Participated in Thursday's class

--- DiceRolling.java
+++ DiceRolling.java
@@ -1,7 +1,12 @@
 package edu.guilford;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceRolling {
     public static void main(String[] args) {
@@ -12,7 +17,7 @@ public class DiceRolling {
         final int NFACES = 100;
         // *final* means that the program cannot change the value
         // use that for constants; and use identifiers with all capital letters
-        
+
         // declare and create an array of dice values
         int[] dieValues = new int[NDICE];
 
@@ -50,19 +55,69 @@ public class DiceRolling {
         int[] count = new int[NBINS];
 
         // We need to know how big each bin is
-        int binSize = NFACES / NBINS; 
+        int binSize = NFACES / NBINS;
         // This works well if NBINS evenly divides NFACES
 
-        // Use a for-each loop to look at each die value and assign it to 
+        // Use a for-each loop to look at each die value and assign it to
         // the appropriate bin
         for (int value : dieValues) {
-          int bin = (value - 1) / binSize; // this is the bin the value goes in
-          // Example: value = 83; (value - 1) = 82; 82 / 100 = 8 in Java
-          count[bin] = count[bin] + 1; // add 1 to the bin value belongs to
+            int bin = (value - 1) / binSize; // this is the bin the value goes in
+            // Example: value = 83; (value - 1) = 82; 82 / 100 = 8 in Java
+            count[bin] = count[bin] + 1; // add 1 to the bin value belongs to
         }
 
         // Print out the result
         System.out.println("Histogram: " + Arrays.toString(count));
+
+        // Now let's work with files!
+
+        // Writing to a file with a FileWriter object
+        // First, we define a File object with the name of the file
+        File file = new File("histogram.txt");
+
+        // Instantiate a FileWriter object that has the task of writing to that file
+        try { // This is a try-catch block
+              // Try this code to see if it runs without serious error
+            FileWriter writer = new FileWriter(file, true); // By default, FileWRiter objects overwrite what's in the
+                                                            // file we created; using "true" as the second parameter
+                                                            // prevents this and instead appends any new data we try to
+                                                            // write
+            writer.write("Bin\tHistogram\n"); // This line writes the strings we pass to it to the file
+
+            // Let's write out the values in the count array using a while loop
+            int bin = 0;
+            while (bin < count.length) {
+                writer.write(bin + "\t" + count[bin] + "\n");
+                bin++;
+            }
+
+            writer.close(); // We have to close the file to flush out everything we wrote to the file
+        } catch (Exception e) {
+            // Do this stuff if specific exception occurs
+            e.printStackTrace();
+        }
+
+        // To read text files that have tokens or lines, we use Scanner
+        // Instantiate a Scanner object that is connected to the file
+        try {
+            Scanner scan = new Scanner(file);
+
+            // We want to read the lines in the file and print them out as long as the file
+            // still has data in it
+            // For a file consisting of lines of data, we use the hasNextLine method of
+            // Scanner, which returns true if there is another line in the file and false if
+            // not
+            while (scan.hasNextLine()) {
+                String theLine = scan.nextLine();
+                String[] theLineArray = theLine.split("\t");
+                System.out.println(Arrays.toString(theLineArray));
+            }
+            scan.close(); // We want to make sure we close scan once we're done with it
+        } catch (FileNotFoundException e) { // Notice how this exception is different from the one in the try-catch
+                                            // block above; the specific exception this try-catch block is worried about
+                                            // is if the file we're trying to read from cannot be found
+            e.printStackTrace();
+        }
 
     }
 }
